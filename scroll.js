cd83c0fd1bbfc99538b8b8022a9813c580255437
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-05-07 21:46:56 +0900

    Delete scroll.js

--- scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-    const arrow = document.getElementById("scroll");
-    const scroll = window.pageYOffset;
-    if (scroll > 100 && getComputedStyle(arrow, null).getPropertyValue('opacity') == 1) {
-        arrow.animate([{opacity: '1'}, {opacity: '0'}], 500);
-        arrow.style.opacity = "0";
-    }
-});
