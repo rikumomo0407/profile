e4966814fbbf0c50103c6ae5953269a37d7cb4c0
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-05-08 14:14:26 +0900

    darkmode.js を更新

--- darkmode.js
+++ darkmode.js
@@ -2,15 +2,16 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const checkToggle = document.getElementById('mode-button');
     const rotateIcon = document.getElementById('rotate');
     let nowRotate = 0;
+    const twitter = document.getElementById('twitter-setting').innerHTML;
     checkToggle.addEventListener('change', function(e) {
         nowRotate += 180;
         rotateIcon.style.transform = 'rotate(' + nowRotate + 'deg)';
         if(e.target.checked) {
             document.body.classList.add('mode-light');
+            twitter = '<div id="twitter-setting"><a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a></div>';
         } else {
             document.body.classList.remove('mode-light');
+            twitter = '<div id="twitter-setting"><a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a></div>';
         }
     });
 });
-
-//<div id="twitter-setting"><a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a></div>
