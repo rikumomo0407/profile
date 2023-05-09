1a8b04999b8f0fae22dfbadfc890cbb650a123df
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-05-10 04:46:57 +0900

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -2,16 +2,20 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const checkToggle = document.getElementById('mode-button');
     const rotateIcon = document.getElementById('rotate');
     let nowRotate = 0;
-    const twitter = document.getElementById('twitter-setting').innerHTML;
+    let twitter = document.getElementById('twitter-setting');
     checkToggle.addEventListener('change', function(e) {
         nowRotate += 180;
         rotateIcon.style.transform = 'rotate(' + nowRotate + 'deg)';
         if(e.target.checked) {
             document.body.classList.add('mode-light');
-            twitter = '<div id="twitter-setting"><a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="light" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a></div>';
+            twitter.innerHTML = '<a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="light" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar">Tweets by rikumomo0407</a>';
         } else {
             document.body.classList.remove('mode-light');
-            twitter = '<div id="twitter-setting"><a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a></div>';
+            twitter.innerHTML = '<a class="twitter-timeline" data-lang="en" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/rikumomo0407?ref_src=twsrc%5Etfw" data-chrome="noheader nofooter noscrollbar transparent">Tweets by rikumomo0407</a>';
         }
+        let script = document.createElement('script');
+        script.src = 'https://platform.twitter.com/widgets.js';
+        script.async = true;
+        twitter.appendChild(script);
     });
 });
