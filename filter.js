aec497d8de807e30b8ed24cce901a8a5ddd6e866
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-05-10 04:47:22 +0900

    Update filter.js

--- filter.js
+++ filter.js
@@ -6,11 +6,11 @@ window.addEventListener("load", ()=>{
 
 function main(data){
 	const parent = document.querySelector('#timeline ul');
-    let filter = document.querySelectorAll('input[type="radio"]');
+    let filter = document.querySelectorAll('#timeline input');
     let icon = "";
     for(let component of data.timeline){
         component.category == "releases" ? icon = ' 🔥' : component.category == "notes" ? icon = ' 📃' : icon = "";
-        parent.insertAdjacentHTML('beforeend', '<li data-category=' + component.category + '><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
+        parent.insertAdjacentHTML('beforeend', '<li><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
     };
     for (let target of filter) {
         target.addEventListener('change', function (){
@@ -20,7 +20,7 @@ function main(data){
             for(let component of data.timeline){
                 if (target.value == 'all' ||  target.value == component.category) {
                     component.category == "releases" ? icon = ' 🔥' : component.category == "notes" ? icon = ' 📃' : icon = "";
-                    parent.insertAdjacentHTML('beforeend', '<li data-category=' + component.category + '><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
+                    parent.insertAdjacentHTML('beforeend', '<li><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
                 };
             };
         });
