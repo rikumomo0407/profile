beb37d580e4a02054213a7570512a12c8bed0da2
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-05-10 23:41:05 +0900

    Update filter.js

--- filter.js
+++ filter.js
@@ -5,8 +5,11 @@ window.addEventListener("load", ()=>{
 });
 
 function main(data){
+    const recommend = document.getElementById('recommend-repo');
+    const popular = document.getElementById('popular-repo');
+    const updated = document.getElementById('updated-repo');
 	const parent = document.querySelector('#timeline ul');
-    let filter = document.querySelectorAll('#timeline input');
+    let filter = document.querySelectorAll('input[type="radio"]');
     let icon = "";
     for(let component of data.timeline){
         component.category == "releases" ? icon = ' 🔥' : component.category == "notes" ? icon = ' 📃' : icon = "";
@@ -14,15 +17,20 @@ function main(data){
     };
     for (let target of filter) {
         target.addEventListener('change', function (){
-            while(parent.firstChild){
-                parent.removeChild(parent.firstChild);
-            }
-            for(let component of data.timeline){
-                if (target.value == 'all' ||  target.value == component.category) {
-                    component.category == "releases" ? icon = ' 🔥' : component.category == "notes" ? icon = ' 📃' : icon = "";
-                    parent.insertAdjacentHTML('beforeend', '<li><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
+            if(target.value == 'recommend' || target.value == 'popular' || target.value == 'updated'){
+                recommend.style.display = popular.style.display = updated.style.display = "none";
+                target.value == 'recommend' ? recommend.style.display = "block" : target.value == 'popular' ? popular.style.display = "block" : updated.style.display = "block";
+            }else{
+                while(parent.firstChild){
+                    parent.removeChild(parent.firstChild);
+                }
+                for(let component of data.timeline){
+                    if(target.value == 'all' ||  target.value == component.category){
+                        component.category == "releases" ? icon = ' 🔥' : component.category == "notes" ? icon = ' 📃' : icon = "";
+                        parent.insertAdjacentHTML('beforeend', '<li><div class="dot"></div><div class="log"><time>' + component.date + '</time>' + icon + '<p class="bubble">' + component.content + '</p></div></li>');
+                    };
                 };
-            };
+            }
         });
     };
 };
